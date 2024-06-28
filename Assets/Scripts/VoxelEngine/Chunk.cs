54aa0ef52883e973add1b1b9ae4d6169e38cb18a
MrPio <valeriomorelli50@gmail.com>
2024-06-28 02:05:42 +0200

    Voxel UV map

--- Assets/Scripts/VoxelEngine/Chunk.cs
+++ Assets/Scripts/VoxelEngine/Chunk.cs
@@ -1,57 +1,78 @@
-using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
-[RequireComponent(typeof(MeshFilter))] // Vertices
-[RequireComponent(typeof(MeshRenderer))] // Light + Material
-[RequireComponent(typeof(MeshCollider))] // Collisions
-public class Chunk : MonoBehaviour
+namespace VoxelEngine
 {
-    public Vector3 chunkPosition;
-
-    private MeshRenderer meshRenderer;
-    private MeshFilter meshFilter;
-    private MeshCollider meshCollider;
-    private Mesh mesh;
-
-    private void Awake()
+    [RequireComponent(typeof(MeshFilter))] // Vertices
+    [RequireComponent(typeof(MeshRenderer))] // Light + Material
+    [RequireComponent(typeof(MeshCollider))] // Collisions
+    public class Chunk : MonoBehaviour
     {
-        meshRenderer = GetComponent<MeshRenderer>();
-        meshFilter = GetComponent<MeshFilter>();
-        meshCollider = GetComponent<MeshCollider>();
-    }
+        [SerializeField] private int chunkSize = 5;
+        [SerializeField] private int chunkHeight = 15;
+        [SerializeField] private int worldChunks = 50;
+        [SerializeField] private int viewChunksDistance = 8;
+        [SerializeField] private int textureAtlasSizeInBlocks = 4;
+        public int WorldBlocks => worldChunks * chunkSize;
+        public float NormalizedBlockTextureSize => 1f / (float)textureAtlasSizeInBlocks;
 
-    private void Start()
-    {
-        var vertexIndex = 0;
-        var vertices = new List<Vector3>();
-        var triangles = new List<int>();
-        var uvs = new List<Vector2>();
+        private MeshRenderer _meshRenderer;
+        private MeshFilter _meshFilter;
+        private MeshCollider _meshCollider;
 
-        // The following can be (a little) more efficiently rewritten using arrays instead of lists
-        // NOTE: Vertices cannot be shared across different faces, otherwise Unity will attempt to
-        // smoothen them and we won't obtain the desired cube crisp edges 
-        for (var p = 0; p < 6; p++)
+        private int _vertexIndex = 0;
+        private readonly List<Vector3> _vertices = new();
+        private readonly List<int> _triangles = new();
+        private readonly List<Vector2> _uvs = new();
+
+
+        private void Awake()
         {
-            for (var i = 0; i < 6; i++)
+            _meshRenderer = GetComponent<MeshRenderer>();
+            _meshFilter = GetComponent<MeshFilter>();
+            _meshCollider = GetComponent<MeshCollider>();
+        }
+
+        private void Start()
+        {
+            for (var y = -10; y < 0; y++)
+            for (var x = -50; x < 50; x++)
+            for (var z = -50; z < 50; z++)
+                AddVoxelDataToChunk(new Vector3(x, y, z));
+            CreateMesh();
+        }
+
+        
+        private void AddVoxelDataToChunk(Vector3 pos)
+        {
+            // The following can be (a little) more efficiently rewritten using arrays instead of lists
+            // NOTE: Vertices cannot be shared across different faces, otherwise Unity will attempt to
+            // smoothen them and we won't obtain the desired cube crisp edges 
+            for (var p = 0; p < 6; p++)
             {
-                var triangleIndex = VoxelData.voxelTris[p, i];
-                vertices.Add(VoxelData.voxelVerts[triangleIndex]);
-                triangles.Add(vertexIndex);
-                uvs.Add(VoxelData.voxelUvs[i]);
-                vertexIndex++;
+                for (var i = 0; i < 6; i++)
+                {
+                    var triangleIndex = VoxelData.VoxelTris[p, i];
+                    _vertices.Add(VoxelData.VoxelVerts[triangleIndex] + pos);
+                    _triangles.Add(_vertexIndex);
+                    _uvs.Add(VoxelData.VoxelUvs[i]);
+                    _vertexIndex++;
+                }
             }
         }
 
-        var mesh = new Mesh
+        private void CreateMesh()
         {
-            vertices = vertices.ToArray(),
-            triangles = triangles.ToArray(),
-            uv = uvs.ToArray()
-        };
-
-        mesh.RecalculateNormals();
-
-        meshFilter.mesh = mesh;
+            var mesh = new Mesh
+            {
+                // indexFormat = IndexFormat.UInt32,
+                vertices = _vertices.ToArray(),
+                triangles = _triangles.ToArray(),
+                uv = _uvs.ToArray()
+            };
+            mesh.RecalculateNormals();
+            _meshFilter.mesh = mesh;
+        }
     }
 }
--- Assets/Scripts/VoxelEngine/VoxelData.cs
+++ Assets/Scripts/VoxelEngine/VoxelData.cs
@@ -1,76 +1,59 @@
 using UnityEngine;
 
-public static class VoxelData
+namespace VoxelEngine
 {
-    public static readonly int ChunkWidth = 5;
-    public static readonly int ChunkHeight = 15;
-    public static readonly int WorldSizeInChunks = 50;
-    public static readonly int ViewDistanceInChunks = 8;
-
-    public static int WorldSizeInBlocks
+    public static class VoxelData
     {
-        get { return WorldSizeInChunks * ChunkWidth; }
+        // We assume all the blocks to have 8 vertices. Stairs, for example make an exception.
+        public static readonly Vector3[] VoxelVerts = new Vector3[8]
+        {
+            new(0.0f, 0.0f, 0.0f),
+            new(1.0f, 0.0f, 0.0f),
+            new(1.0f, 1.0f, 0.0f),
+            new(0.0f, 1.0f, 0.0f),
+            new(0.0f, 0.0f, 1.0f),
+            new(1.0f, 0.0f, 1.0f),
+            new(1.0f, 1.0f, 1.0f),
+            new(0.0f, 1.0f, 1.0f),
+        };
+
+        // Each int point to one Vector3 of the voxelVerts list
+        // The order of each triangle's vertices is constrained by:
+        // - Clockwise order to have an outgoing normal vector
+        // - The permutation must be the same for all 12 triangles to get a correct UV mapping
+        public static readonly int[,] VoxelTris = new int[6, 6]
+        {
+            { 0, 3, 1, 1, 3, 2 }, // Back Face
+            { 5, 6, 4, 4, 6, 7 }, // Front Face
+            { 3, 7, 2, 2, 7, 6 }, // Top Face
+            { 1, 5, 0, 0, 5, 4 }, // Bottom Face
+            { 4, 7, 0, 0, 7, 3 }, // Left Face
+            { 1, 2, 5, 5, 2, 6 } // Right Face
+        };
+
+
+        // UVs are applied by assigning a normalized texture pixel value to the voxel face.
+        // The order of the UV Vector2 list matches the order of the Vector3 vertices list.
+        // Now that the order is the same for each face, we can write the lookup table for just one face.
+        public static readonly Vector2[] VoxelUvs = new Vector2[6]
+        {
+            new(0.0f, 0.0f),
+            new(0.0f, 1.0f),
+            new(1.0f, 0.0f),
+            new(1.0f, 0.0f),
+            new(0.0f, 1.0f),
+            new(1.0f, 1.0f)
+        };
+
+        // The order matches the faces of the VoxelTris faces.
+        public static readonly Vector3[] FaceChecks = new Vector3[6]
+        {
+            new(0.0f, 0.0f, -1.0f), // Back Face
+            new(0.0f, 0.0f, 1.0f), // Front Face
+            new(0.0f, 1.0f, 0.0f), // Top Face
+            new(0.0f, -1.0f, 0.0f), // Bottom Face
+            new(-1.0f, 0.0f, 0.0f), // Left Face
+            new(1.0f, 0.0f, 0.0f) // Right Face
+        };
     }
-
-    public static readonly int TextureAtlasSizeInBlocks = 4;
-
-    public static float NormalizedBlockTextureSize
-    {
-        get { return 1f / (float)TextureAtlasSizeInBlocks; }
-    }
-
-    // We assume all the blocks to have 8 vertices. Stairs, for example make an exception.
-    public static readonly Vector3[] voxelVerts = new Vector3[8]
-    {
-        new(0.0f, 0.0f, 0.0f),
-        new(1.0f, 0.0f, 0.0f),
-        new(1.0f, 1.0f, 0.0f),
-        new(0.0f, 1.0f, 0.0f),
-        new(0.0f, 0.0f, 1.0f),
-        new(1.0f, 0.0f, 1.0f),
-        new(1.0f, 1.0f, 1.0f),
-        new(0.0f, 1.0f, 1.0f),
-    };
-
-    // Each int point to one Vector3 of the voxelVerts list
-    // The order of each triangle's vertices is constrained by:
-    // - Clockwise order to have an outgoing normal vector
-    // - The permutation must be the same for all 12 triangles to get a correct UV mapping
-    public static readonly int[,] voxelTris = new int[6,6] {
-
-        {0, 3, 1, 1, 3, 2}, // Back Face
-        {5, 6, 4, 4, 6, 7}, // Front Face
-        {3, 7, 2, 2, 7, 6}, // Top Face
-        {1, 5, 0, 0, 5, 4}, // Bottom Face
-        {4, 7, 0, 0, 7, 3}, // Left Face
-        {1, 2, 5, 5, 2, 6} // Right Face
-
-    };
-    
-    
-    // UVs are applied by assigning a normalized texture pixel value to the voxel face.
-    // The order of the UV Vector2 list matches the order of the Vector3 vertices list.
-    // Now that the order is the same for each face, we can write the lookup table for just one face.
-    public static readonly Vector2[] voxelUvs = new Vector2[6]
-    {
-        new(0.0f, 0.0f),
-        new(0.0f, 1.0f),
-        new(1.0f, 0.0f),
-        new(1.0f, 0.0f),
-        new(0.0f, 1.0f),
-        new(1.0f, 1.0f)
-        
-    };
-
-    public static readonly Vector3[] faceChecks = new Vector3[6]
-    {
-        new(0.0f, 0.0f, -1.0f),
-        new(0.0f, 0.0f, 1.0f),
-        new(0.0f, 1.0f, 0.0f),
-        new(0.0f, -1.0f, 0.0f),
-        new(-1.0f, 0.0f, 0.0f),
-        new(1.0f, 0.0f, 0.0f)
-    };
-
-
 }
