52d7689b91c7ad4982675465556232f27de180cc
MrPio <valeriomorelli50@gmail.com>
2024-07-03 22:37:44 +0200

    Block Placing intersection prevention algorithm rewritten.

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -1,17 +1,19 @@
+using Unity.Mathematics;
 using UnityEngine;
 using VoxelEngine;
 
 public class CameraMovement : MonoBehaviour
 {
+    [SerializeField] private Transform highlightBlock, placeBlock;
+    [SerializeField] private CharacterController characterController;
     public float sensitivity;
     public Transform player;
     private float _rotX, _rotY;
-    [SerializeField] private Transform highlightBlock, placeBlock;
     public float checkIncrement = 0.1f;
     public float reach = 8;
     private WorldManager _wm;
     private Transform _transform;
-    private bool _canDig=true, _canPlace;
+    private bool _canDig, _canPlace = true;
 
     public bool CanDig
     {
@@ -77,7 +79,7 @@ public class CameraMovement : MonoBehaviour
         var angleFactor = Mathf.Sin(Mathf.Deg2Rad * transform.eulerAngles.x);
         var headOffset = angleFactor < 0 ? -angleFactor * .3f : 0;
         angleFactor = angleFactor < 0 ? 0 : angleFactor;
-        var minDistance = _canPlace ? 1.1f + 1f * Mathf.Pow(angleFactor,3.2f)-headOffset : 0;
+        var minDistance = _canPlace ? 1.04f + 1f * Mathf.Pow(angleFactor, 4.85f) - headOffset : 0;
         var lastPos = Vector3Int.FloorToInt(_transform.position);
         for (var lenght = checkIncrement; lenght < reach; lenght += checkIncrement)
         {
@@ -94,10 +96,14 @@ public class CameraMovement : MonoBehaviour
             if (_canPlace && blockType is { isSolid: true })
             {
                 var newPos = lastPos + Vector3.one * 0.5f;
+                var characterPos = characterController.transform.position;
+                var distanceXZ = Vector3.Distance(new Vector3(newPos.x, 0, newPos.z),
+                    new Vector3(characterPos.x, 0, characterPos.z));
+                var distanceY = math.abs(newPos.y - characterPos.y);
+                var intersect = distanceXZ < characterController.radius+0.25f && distanceY < characterController.height-0.2f;
                 placeBlock.position = newPos;
-                print($"{Vector3.Distance(placeBlock.position, transform.position)}/{minDistance}");
-                placeBlock.gameObject.SetActive(
-                    Vector3.Distance(newPos, _transform.position) > minDistance);
+                print($"{distanceXZ}/{distanceY}");
+                placeBlock.gameObject.SetActive(!intersect);
                 highlightBlock.gameObject.SetActive(false);
                 return;
             }
