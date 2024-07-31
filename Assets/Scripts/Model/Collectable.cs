0e3408e32517257e0ef1b13a39b8b3712ec668c8
Valerio Morelli <valeriomorelli50@gmail.com>
2024-07-31 19:07:58 +0200

    Collectables introduced

---
+++ Assets/Scripts/Model/Collectable.cs
@@ -0,0 +1,19 @@
+﻿namespace Model
+{
+    public class Collectable
+    {
+        public enum CollectableType
+        {
+            Weapon, Ammo, Hp
+        }
+
+        public CollectableType Type;
+        public Weapon Item;
+
+        public Collectable(CollectableType type, Weapon item)
+        {
+            Type = type;
+            Item = item;
+        }
+    }
+}
